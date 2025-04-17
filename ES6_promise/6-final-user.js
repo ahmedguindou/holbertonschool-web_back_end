da416b37dec1241fdfaf1198665554ff25527070
ahmedguindou <guindouahmed@gmail.com>
2025-04-17 15:23:22 +0100

    6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,19 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(firstName, lastName);
-  const upload = uploadPhoto(fileName);
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
 
-  return Promise.allSettled([signUp, upload]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: result.status,
-        value: result.value,
-      };
-    }
-    return {
+  return Promise.allSettled(promises)
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.reason,
-    };
-  }));
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    })));
 }
