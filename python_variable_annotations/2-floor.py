cac273e1d6303e4dc3cd5ffed03d5e8021a6a77c
ahmedguindou <guindouahmed@gmail.com>
2025-04-28 17:52:33 +0100

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module containing a function to compute the floor of a floating-point number.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number as an integer.
+    """
+    return int(n // 1)
