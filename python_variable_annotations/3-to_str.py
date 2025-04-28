97be71921489dd1ca9283f247e9bbe68eb26033f
ahmedguindou <guindouahmed@gmail.com>
2025-04-28 17:53:54 +0100

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module containing a function to convert a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to its string representation.
+    """
+    return str(n)
