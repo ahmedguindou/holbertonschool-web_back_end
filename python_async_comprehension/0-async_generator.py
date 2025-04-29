4858d63dc051afb40d2592bd6720b9c7b26ee54b
ahmedguindou <guindouahmed@gmail.com>
2025-04-29 14:34:38 +0100

    0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Yield a random float between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
