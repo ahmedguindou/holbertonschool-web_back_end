51c3a90bb7901c87bf2807dfea9933422384d53c
ahmedguindou <guindouahmed@gmail.com>
2025-05-04 19:17:40 +0100

    1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# """Module containing an asynchronous comprehension function"""
+"""Module containing an asynchronous comprehension function"""
 
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
