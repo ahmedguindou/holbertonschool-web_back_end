0525221f8aacddd7a6040433491257f979ec4187
ahmedguindou <guindouahmed@gmail.com>
2025-05-22 18:27:26 +0100

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
   console.log(`Your name is: ${name}`);
+  process.stdin.end();
 });
 
 process.on('exit', () => {
